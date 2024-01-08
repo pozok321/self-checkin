1d68d61e278c2cd21150ea653aa3c3cfb3efea7d
unknown <valeryanr18@gmail.com>
2024-01-08 21:58:51 +0700

    new login page

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import './assets/main.css'
-import Swal from 'sweetalert2'
+import "./assets/main.css";
+import Swal from "sweetalert2";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "bootstrap/dist/css/bootstrap.css";
+import bootstrap from "bootstrap/dist/js/bootstrap";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import ScanView from '../views/ScanView.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import ScanView from "../views/ScanView.vue";
+import LoginView from "../views/LoginView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'thescan',
+      path: "/",
+      name: "loginpage",
+      component: LoginView,
+    },
+    {
+      path: "/thescan",
+      name: "thescan",
       component: ScanView,
     },
-  ]
-})
+  ],
+});
 
-export default router
+export default router;
