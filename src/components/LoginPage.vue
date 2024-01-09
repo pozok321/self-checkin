9e24da78119dd3cdbdb82011f5cd5738825a7599
valeryan <valeryanr18@gmail.com>
2024-01-09 16:48:46 +0700

    new login page

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,44 +1,26 @@
 <template>
   <div class="login-page bg-light">
     <div class="container">
-      <div class="row">
-        <div class="col-lg-10 offset-lg-1">
-          <div class="bg-white shadow rounded">
-            <div class="row">
-              <div class="col-md-7 pe-0">
-                <div class="form-left h-100 py-5 px-5">
-                  <!-- <form class="row g-4"> -->
-                  <div class="col-12">
-                    <label>Event KEY<span class="text-danger">*</span></label>
-                    <div class="input-group">
-                      <div class="input-group-text"><i class="bi bi-person-fill"></i></div>
-                      <input type="number" class="form-control" placeholder="Enter Event Key"
-                        v-model="obj.events_token" />
-                    </div>
-                  </div>
-
-                  <div class="col-12">
-                    <label>Event ID<span class="text-danger">*</span></label>
-                    <div class="input-group">
-                      <div class="input-group-text"><i class="bi bi-lock-fill"></i></div>
-                      <input type="number" class="form-control" placeholder="Enter Event ID" v-model="obj.events_id" />
-                    </div>
-                  </div>
-                  <div class="col-12">
-                    <button class="btn btn-primary px-4 float-end mt-4" @click="login_page()">login</button>
-                  </div>
-                  <!-- </form> -->
-                </div>
-              </div>
-              <div class="col-md-5 ps-0 d-none d-md-block">
-                <div class="form-right h-100 bg-darkblue text-white text-center pt-5">
-                  <i class="bi bi-bootstrap"></i>
-                  <h2 class="fs-1">Self Checkin Login</h2>
-                </div>
-              </div>
-            </div>
+      <div class="h-100 width-50">
+        <!-- <form class="row g-4"> -->
+        <div class="form-group">
+          <label>Event KEY<span class="text-danger">*</span></label>
+          <div class="input-group">
+            <div class="input-group-text"><i class="bi bi-person-fill"></i></div>
+            <input type="number" class="form-control" placeholder="Enter Event Key" v-model="obj.events_token" />
           </div>
         </div>
+        <div class="form-group">
+          <label>Event ID<span class="text-danger">*</span></label>
+          <div class="input-group">
+            <div class="input-group-text"><i class="bi bi-lock-fill"></i></div>
+            <input type="number" class="form-control" placeholder="Enter Event ID" v-model="obj.events_id" />
+          </div>
+        </div>
+        <div class="form-group">
+          <button class="button-login mt-4" @click="login_page()">login</button>
+        </div>
+        <!-- </form> -->
       </div>
     </div>
   </div>
@@ -112,5 +94,34 @@
 
   .bg-darkblue {
     background-color: #1a283d;
+  }
+
+  .rounded-top-left {
+    position: relative;
+    top: 0;
+    right: 0;
+  }
+
+  .rounded-bottom-left {
+    position: relative;
+    bottom: 0;
+    left: 0;
+  }
+
+  .width-50 {
+    margin: auto;
+    width: 50%;
+  }
+
+  .button-login{
+    width: 100%;
+    background-color: #163C56;
+    color: #fff;
+    font-family: Helvetica;
+    border-radius: 20px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    font-size: 16pt;
+    font-weight: bold;
   }
 </style>
